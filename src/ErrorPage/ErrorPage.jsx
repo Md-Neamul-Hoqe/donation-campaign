f1da8faa816283b2bde9c5227f377c2da296e3fe
Md Neamul Hoqe <hoqe1997@gmail.com>
2023-09-26 09:51:41 +0600

    Pie Chart responsive to all devices [min 320px]

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div className="min-w-screen flex items-center justify-center">
-      <h1 className="text-5xl font-extrabold">Oops!!!</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-6xl md:text-9xl font-extrabold">Oops!!!</h1>
       <p className="my-10">Sorry! Not Found</p>
-      <Link className="btn btn-accent" to={"/"}>
+      <Link className="btn btn-accent text-white" to={"/"}>
         Go Back To Home Page
       </Link>
     </div>
--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,27 +1,65 @@
 import { useEffect, useState } from "react";
-import { PieChart, Pie, Cell } from "recharts";
+import { PieChart, Pie, Cell, Tooltip } from "recharts";
 
 const Statistics = () => {
   /* load the total donations */
+  const [totalDonations, setTotalDonations] = useState(0);
+  const [parameters, setParameters] = useState({});
+  const [theWindowsWidthState, setTheWindowsWidthState] = useState(0);
+
   useEffect(() => {
     fetch("/donation.json")
       .then((res) => res.json())
       .then((data) => setTotalDonations(data?.length));
   }, []);
 
-  const [totalDonations, setTotalDonations] = useState(0);
+  window.addEventListener("resize", () => {
+    setTheWindowsWidthState(screen.width);
+  });
 
   const LSDonationsString = localStorage.getItem("donations") || [];
+
+  /* Get Your Donations */
   const myDonation = JSON.parse(LSDonationsString).length;
 
+  /* Set the Getting values */
   const data = [
-    { name: "Your Donations", value: (myDonation * 100) / 12 },
-    { name: "Total Donations", value: (totalDonations * 100) / 12 },
+    { name: "Your Donations", value: myDonation },
+    { name: "Total Donations", value: totalDonations },
   ];
 
-  const COLORS = ["#FF0000", "#00FF00"];
+  /* set colors */
+  const COLORS = ["#00C49F", "#FF444A"];
 
   const RADIAN = Math.PI / 180;
+  // let theWindowsWidth = screen.width;
+
+  /* to responsive the pie chart with page */
+  useEffect(() => {
+    if (totalDonations !== 0) {
+      if (theWindowsWidthState < 768) {
+        const newParameters = {
+          outerRadiusOfPie: 200,
+          PieHeight: 650,
+          PieWidth: 410,
+          cxInPieChart: 200,
+          cyInPieChart: 400,
+        };
+        setParameters(newParameters);
+      } else if (theWindowsWidthState > 1556) {
+        const newParameters = {
+          outerRadiusOfPie: 300,
+          PieHeight: 1000,
+          PieWidth: 410,
+          cxInPieChart: 200,
+          cyInPieChart: 500,
+        };
+        setParameters(newParameters);
+      }
+    }
+  }, [theWindowsWidthState, totalDonations]);
+
+  /* label for each area */
   const myLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
     const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
@@ -38,22 +76,45 @@ const Statistics = () => {
       </text>
     );
   };
+
   return (
-    <div>
-      <PieChart width={800} height={800}>
-        <Pie
-          data={data}
-          cx={600}
-          cy={500}
-          labelLine={false}
-          label={myLabel}
-          outerRadius={150}
-          dataKey="value">
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-          ))}
-        </Pie>
-      </PieChart>
+    <div className="font-bold flex flex-col items-center">
+      <div className="grid grid-cols-1">
+        <PieChart
+          id="pieChartId"
+          className="mx-auto"
+          width={parameters.PieWidth}
+          height={parameters.PieHeight}>
+          <Pie
+            className="text-xl md:text-2xl lg:text-4xl"
+            data={data}
+            cx={parameters.cxInPieChart}
+            cy={parameters.cyInPieChart}
+            labelLine={false}
+            label={myLabel}
+            outerRadius={parameters.outerRadiusOfPie}
+            dataKey="value">
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+          {/* <Tooltip></Tooltip> */}
+        </PieChart>
+      </div>
+
+      <div className="flex max-sm:flex-col whitespace-nowrap gap-5 md:gap-10 mb-10 text-xs md:text-base xl:text-xl 2xl:text-5xl">
+        <p className="flex items-center gap-5">
+          Your Donation{" "}
+          <span className="w-32 xl:w-40 h-2 xl:h-3 bg-[#00C49F]"></span>
+        </p>
+        <p className="flex items-center gap-5">
+          Total Donation{" "}
+          <span className="w-32 xl:w-40 h-2 xl:h-3 bg-[#FF444A]"></span>
+        </p>
+      </div>
     </div>
   );
 };
